e26b270b9425e0eee35559a5071a190c56c8aa87
ahhon <polygon2000x@gmail.com>
2016-02-12 04:30:56 +0700

    cache

--- wikidic.sh
+++ wikidic.sh
@@ -3,11 +3,21 @@
 set -u
 
 urlBase="http://en.wiktionary.org/wiki"
+pager="cat"
+dirCache="/tmp/.wikidic_cache"
+
+rmScum() {
+	sed '/^Contents\>/,/^[^[:space:]]/d; /^Translation/,/^[^[:space:]]/d; /^Navigation menu/,$d; /^Anagrams/,/^[^[:space:]]/d; /^Statistics/,/^[^[:space:]]/d; /^Related terms/,/^[^[:space:]]/d; /^Derived terms/,/^[^[:space:]]/d;'
+	
+}
+
 
 if [ $# -lt 1 ]; then
 	echo give a word >&2
 	exit 1
 fi
+
+mkdir -p "$dirCache"
 
 flgNoLess=0
 
@@ -23,8 +33,13 @@ done
 phrase="`echo ${arr[@]}`"
 url="$urlBase/$phrase"
 
-if [ $flgNoLess -ne 0 ]; then
-	lynx -dump "$url"
-else
-	lynx -dump "$url" | less
+if [ $flgNoLess -eq 0 ]; then
+	pager=less
 fi
+
+fileCache="$dirCache/$phrase"
+if [ '!' -e "$fileCache" ]; then
+	lynx -dump "$url" > "$fileCache"
+fi
+
+cat "$fileCache" | rmScum | "$pager"
