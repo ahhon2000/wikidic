55e9a8051ed171165ee87dba9a962a894a6a96ee
ahhon <polygon2000x@gmail.com>
2019-04-30 21:32:56 +0700

    added codic.sh

---
+++ codic.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#
+# USAGE: wikidic WORD
+#	 or:
+#	 wikidic -c   # clear cache
+
+set -u
+
+urlBase="https://www.collinsdictionary.com/dictionary/english"
+pager="cat"
+dirCache="$HOME/.codic_cache"
+TIMEOUT_SECONDS=25
+pathScript="`readlink -f "$0"`"
+dirScript="`dirname "$pathScript"`"
+#preprocess="$dirScript/oxdicPreprocess"
+preprocess="cat"
+
+rmScum() {
+	sed "1,/^Definition of '/d" | \
+		sed '/^[()*[:space:]]\+$/d' | \
+		sed '/^[[:space:]]*Word Frequency[[:space:]]*$/d' | \
+		sed '/^Translations for\>/,$d' | sed '/^Trends of\>/,$d'
+}
+
+if [ $# -eq 1 ] && [ "$1" = '-c' ]; then
+	# clear cache
+	find "$dirCache" -mindepth 1 -maxdepth 1 -delete
+	exit 0
+fi
+
+if [ $# -lt 1 ]; then
+	echo give a word >&2
+	exit 1
+fi
+
+mkdir -p "$dirCache"
+
+flgNoLess=0
+
+arr=()
+for a; do
+	if [ "$a" = '-n' ]; then
+		flgNoLess=1
+		continue
+	fi
+	arr+=("$a")
+done
+
+phrase="`echo ${arr[@]}`"
+url="$urlBase/$phrase"
+
+if [ $flgNoLess -eq 0 ]; then
+	pager=less
+fi
+
+fileCache="$dirCache/$phrase"
+if [ '!' -e "$fileCache" ]; then
+	timeout "$TIMEOUT_SECONDS" lynx -dump "$url" > "$fileCache"
+fi
+
+cat "$fileCache" | rmScum | "$preprocess" | "$pager"
