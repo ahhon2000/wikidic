c7e7e07a9d103188836c6cdcabc585de20c1ab19
ahhon <polygon2000x@gmail.com>
2020-10-09 19:19:49 +0700

    completed mwdic (Python)

--- DictionaryAppMW.py
+++ DictionaryAppMW.py
@@ -1,4 +1,5 @@
 import os
+import re
 from pathlib import Path
 
 from DictionaryApp import DictionaryApp
@@ -9,3 +10,19 @@ class DictionaryAppMW(DictionaryApp):
 
         self.urlBase = "https://www.merriam-webster.com/dictionary"
         self.cacheDir = Path(os.environ.get('HOME')) / '.mwdic_cache'
+
+    def processLines(self):
+        ols = self.lines
+
+        def flt1(ls):
+            for i, l in enumerate(ls):
+                if re.search(r'^[^\s]', l): return ls[i:]
+            return []
+
+        def flt2(ls):
+            for i, l in enumerate(ls):
+                if re.search(r'^\s*Dictionary Entries near', l): return ls[0:i]
+            return ls
+        
+        for flt in (flt1, flt2): ols = flt(ols)
+        self.outputLines = list(ols)
